11d82e78c9032468db7d323931d1adb54a521ecb
Nilton Jhonas <niltonnjhonas@gmail.com>
2024-11-05 19:41:46 -0300

    Jokenpo.py

--- Jokenpo.py
+++ Jokenpo.py
@@ -3,21 +3,14 @@
 from time import sleep # Função que executa determinado comando depois de determina tempo.
 from os import system, name # Funções para limpar o terminal.
 from sys import exit # Função para finalizar código.
-from random import randint # Função para escolher um número aleatório (usamos para converte-lo em uma condição).
+from random import choice # Função para escolher um número aleatório (usamos para converte-lo em uma condição).
 
 # Função do jogo
 def jogo_jokenpo():
 
-# Escolhendo um número aleatório (entre 1-3)
-    escolha_bot = randint(1, 3)
-
-# Convertendo os números para as possibilidades do jogo
-    if escolha_bot == 1:
-        escolha_bot = 'pedra'
-    elif escolha_bot == 2:
-        escolha_bot = 'papel'
-    elif escolha_bot == 3:
-        escolha_bot = 'tesoura'
+# Escolhendo um valor da lista e o inserindo na variável
+    possibilidades = ['pedra','papel','tesoura']
+    escolha_computador = choice(possibilidades)
 
 # Capa do jogo
     print('----------------------')
@@ -38,30 +31,28 @@ def jogo_jokenpo():
 # Imprime a escolha do computador
     print('\n\033[35mO computador escolhe... ')
     sleep(2)
-    print('{}\n'.format(escolha_bot.capitalize()))
+    print('{}\n'.format(escolha_computador.capitalize()))
     sleep(2)
 
 # Verifica se houve um empate...
-    if escolha == 'pedra' and escolha_bot == 'pedra' or escolha == 'papel' and escolha_bot == 'papel' or escolha == 'tesoura' and escolha_bot == 'tesoura':
-        print('\033[34m{}\033[m - \033[35m{}\033[0;7m\nEmpate!\033[m\n'.format(escolha.capitalize(), escolha_bot.capitalize()))
+    if escolha == 'pedra' and escolha_computador == 'pedra' or escolha == 'papel' and escolha_computador == 'papel' or escolha == 'tesoura' and escolha_computador == 'tesoura':
+        print('\033[34m{}\033[m - \033[35m{}\n\n   \033[0;7mEmpate!\033[m\n'.format(escolha.capitalize(), escolha_computador.capitalize()))
 
 # Verifica se o usuário venceu...
-    elif escolha == 'pedra' and escolha_bot == 'tesoura' or escolha == 'papel' and escolha_bot == 'pedra' or escolha == 'tesoura' and escolha_bot == 'papel':
-        print('\033[34m{}\033[m - \033[35m{}\033[32m\nVocê venceu!\033[m\n'.format(escolha.capitalize(), escolha_bot.capitalize()))
+    elif escolha == 'pedra' and escolha_computador == 'tesoura' or escolha == 'papel' and escolha_computador == 'pedra' or escolha == 'tesoura' and escolha_computador == 'papel':
+        print('\033[34m{}\033[m - \033[35m{}\033[32m\n\nVocê venceu!\033[m\n'.format(escolha.capitalize(), escolha_computador.capitalize()))
 
 # Verifica se o computador venceu...
-    elif escolha == 'pedra' and escolha_bot == 'papel' or escolha == 'papel' and escolha_bot == 'tesoura' or escolha == 'tesoura' and escolha_bot == 'papel':
-        print('\033[34m{}\033[m - \033[35m{}\033[31m\nVocê perdeu!\033[m\n'.format(escolha.capitalize(), escolha_bot.capitalize()))
+    elif escolha == 'pedra' and escolha_computador == 'papel' or escolha == 'papel' and escolha_computador == 'tesoura' or escolha == 'tesoura' and escolha_computador == 'papel':
+        print('\033[34m{}\033[m - \033[35m{}\033[31m\n\nVocê perdeu!\033[m\n'.format(escolha.capitalize(), escolha_computador.capitalize()))
     
 # Executa-se caso o usuário digite um valor inválido
     else:
         print('\n\033[31mDigite um valor válido!\033[m')
     print('\033[31m----- FIM de JOGO -----\033[35m\n')
-    sleep(3)
-    print('  Reiniciando...\n')
     sleep(2)
     print('----- Aguarde -----\033[m')
-    sleep(5)
+    sleep(5.5)
     system('cls' if name == 'nt' else 'clear')
 
 # Estrutura de decisão para o jogo recomeçar (dependendo da decisão do usuário)
@@ -73,4 +64,3 @@ while True:
         exit()
     sleep(2.3)
     system('cls' if name == 'nt' else 'clear')
-
