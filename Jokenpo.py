21c065143545b53e17cb3bacf3ecd654377f507f
Nilton Jhonas <niltonnjhonas@gmail.com>
2024-11-11 09:58:58 -0300

    Update Jokenpo.py

--- Jokenpo.py
+++ Jokenpo.py
@@ -25,43 +25,42 @@ def jogo_jokenpo():
 
 ----------------------''')
 # Opção de escolha do usuário
-    escolha = str(input('\033[34mEu escolho:\n'))
-    escolha = escolha.lower().replace(' ', '') # Deixa as letras em minúsculo e retira os espaços, para minimizar os erros por escrita
-# Imprime a escolha do computador
+    escolha = str(input('\033[34mEu escolho:\n')).lower().replace(' ', '')
+    if escolha not in ['pedra', 'papel', 'tesoura']:
+        print('\n\033[31mDigite um valor válido!\033[m\n')
+        return
     print('\n\033[35mO computador escolhe... ')
     sleep(2)
     print('{}\n'.format(escolha_computador.capitalize()))
     sleep(2)
-
+    match (escolha, escolha_computador):
 # Verifica se houve um empate...
-    if escolha == 'pedra' and escolha_computador == 'pedra' or escolha == 'papel' and escolha_computador == 'papel' or escolha == 'tesoura' and escolha_computador == 'tesoura':
-        print('\033[34m{}\033[m - \033[35m{}\n\n   \033[0;7mEmpate!\033[m\n'.format(escolha.capitalize(), escolha_computador.capitalize()))
+        case ('pedra', 'pedra') | ('papel', 'papel') | ('tesoura', 'tesoura'):
+            print('\033[34m{}\033[m - \033[35m{}\n\n   \033[0;7mEmpate!\033[m\n'.format(escolha.capitalize(), escolha_computador.capitalize()))
 
 # Verifica se o usuário venceu...
-    elif escolha == 'pedra' and escolha_computador == 'tesoura' or escolha == 'papel' and escolha_computador == 'pedra' or escolha == 'tesoura' and escolha_computador == 'papel':
-        print('\033[34m{}\033[m - \033[35m{}\033[32m\n\nVocê venceu!\033[m\n'.format(escolha.capitalize(), escolha_computador.capitalize()))
+        case ('pedra', 'tesoura') | ('papel', 'pedra') | ('tesoura', 'papel'):
+            print('\033[34m{}\033[m - \033[35m{}\033[32m\n\nVocê venceu!\033[m\n'.format(escolha.capitalize(), escolha_computador.capitalize()))
 
 # Verifica se o computador venceu...
-    elif escolha == 'pedra' and escolha_computador == 'papel' or escolha == 'papel' and escolha_computador == 'tesoura' or escolha == 'tesoura' and escolha_computador == 'papel':
-        print('\033[34m{}\033[m - \033[35m{}\033[31m\n\nVocê perdeu!\033[m\n'.format(escolha.capitalize(), escolha_computador.capitalize()))
+        case ('pedra', 'papel') | ('papel', 'tesoura') | ('tesoura', 'papel'):
+            print('\033[34m{}\033[m - \033[35m{}\033[31m\n\nVocê perdeu!\033[m\n'.format(escolha.capitalize(), escolha_computador.capitalize()))
     
-# Executa-se caso o usuário digite um valor inválido
-    else:
-        print('\n\033[31mDigite um valor válido!\033[m')
     print('\033[31m----- FIM de JOGO -----\033[35m\n')
-    sleep(2)
+    sleep(2.2)
     print('----- Aguarde -----\033[m')
-    sleep(5.5)
+    sleep(3.5)
     system('cls' if name == 'nt' else 'clear')
 
 # Estrutura de decisão para o jogo recomeçar (dependendo da decisão do usuário)
 while True:
     jogo_jokenpo() # Inicia o jogo
     resposta = input('\033[33mDeseja jogar novamente? \033[m(\033[32mS\033[m\033[m/\033[31mN\033[m):  ').upper().replace(' ', '')
-    if resposta == 'S' or resposta == 'SIM': # Se a variável "resposta" for diferente de "S" ou "SIM", finaliza o programa
-        print('\033[35m\n----- Recomeçando -----\033[m')
-        sleep(2.3)
-        system('cls' if name == 'nt' else 'clear')
-    else:
-        print('\n\033[36m--- Até mais! ---\033[m\n')
-        break
+    match resposta:
+        case 'S' | 'SIM': # Caso resposta for "S" ou "SIM" recomeça o programa.
+            print('\033[35m\n----- Recomeçando -----\033[m')
+            sleep(1.7)
+            system('cls' if name == 'nt' else 'clear')
+        case _: # Caso não, finaliza a repetição.
+            print('\n\033[36m--- Até mais! ---\033[m\n')
+            break
